a9e3e059f0b0106732638634b2d7e6555adf506c
Long Hoang <longhoang12212@gmail.com>
2022-11-17 09:53:19 +0700

    LinkedList pack

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,13 +1,12 @@
+import java.util.LinkedList;
+
 public class LinkedListTest {
     public static void main(String[] args) {
         System.out.println("/=/=/=/= TESTING /=/=/=/=");
-        LinkedList ll = new LinkedList(10);
-        ll.addFirst(11);
-        ll.addFirst(12);
-        ll.addFirst(13);
-
-        ll.add(4,9);
-        ll.add(4,9);
-        ll.printList();
+        //LinkedList ll = new LinkedList(10);
+        LinkedList llnew = new LinkedList();
+        LinkedList linked = new LinkedList();
+        llnew.add(0, "Baga");
+        System.out.println(llnew);
     }
 }
